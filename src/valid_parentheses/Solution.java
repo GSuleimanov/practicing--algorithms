2272ad606b97cb20475d08584f5f0bc0b2f4c62e
GSuleimanov <suleimanov.dev@gmail.com>
2022-12-03 03:06:36 +0300

    valid_parentheses with Optional

--- src/valid_parentheses/Solution.java
+++ src/valid_parentheses/Solution.java
@@ -1,13 +1,8 @@
 package valid_parentheses;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /*
  * @lc app=leetcode id=20 lang=java
@@ -17,43 +12,24 @@ import java.util.stream.Stream;
 
 // @lc code=start
 class Solution {
-    private static char[] closingBrackets = new char[] {')',']','}'};
-    private static char[] openingBrackets = new char[] {'(','[','{'};
-    LinkedList<Character> linkedList = new LinkedList<>();
+    private static int[] opening = new int[] {40,91,123};
+    private static int[] closing = new int[] {41,93,125};
+    LinkedList<Integer> linkedList = new LinkedList<>();
 
-    public static void main(String[] args) {
-        String s1 = "()";
-        String s2 = "()[]{}";
-        String s3 = "(]";
-        String s4 = "]";
-        String s5 = "";
-        // assert new Solution().isValid(s1);
-        // assert new Solution().isValid(s2);
-        // assert !new Solution().isValid(s3);
-        System.out.println(new Solution().isValid(s1));
-        System.out.println(new Solution().isValid(s2));
-        System.out.println(new Solution().isValid(s3));
-        System.out.println(new Solution().isValid(s4));
-        System.out.println(new Solution().isValid(s5));
-    }
-
-    public boolean isValid(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (bracketsMatches(ch)) linkedList.removeLast();
-            else linkedList.add(ch);
-        }
-        System.out.println(linkedList);
+    public boolean isValid(final String s) {
+        s.chars().forEach(this::addOrDelete);
         return linkedList.isEmpty();
     }
 
-    private boolean bracketsMatches(char bracket) {
-        for (int i = 0; i < closingBrackets.length; i++)
-            if (bracket == closingBrackets[i]
-                    && !linkedList.isEmpty()
-                    && linkedList.getLast() == openingBrackets[i])
-                return true;
-        return false;
+    private void addOrDelete(int bracket) {
+        Optional.ofNullable(linkedList.peekLast())
+            .filter(l -> l.equals(opening[getIdxIfClosing(bracket)]))
+            .ifPresentOrElse(x -> linkedList.removeLast(), () -> linkedList.add(bracket));
+    }
+
+    private int getIdxIfClosing(int bracket) {
+        return IntStream.range(0, closing.length)
+                .filter(i -> closing[i] == bracket).findAny().orElse(-1);
     }
 }
 // @lc code=end
